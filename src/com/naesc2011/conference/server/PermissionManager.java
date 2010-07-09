6db0774fddfaec610b1fdd50c73d3ca662a1e022
jjhatf02 <jjhatf02@c9196558-8581-6202-ea55-1f6dc3d4705e>
2010-07-09 23:29:39 +0000

    Implemented a PermissionManager that is able to use the built in authentication (User) along with a custom object (PermissionUserInstance) to specific a specific level of access that the user is granted.  New users are added to the datastore and existing users have their permission level looked up.  Currently all users default to the "Authenticated" level, but it will be possible to add additional levels of permissions that are set and made persistant by the datastore.

---
+++ src/com/naesc2011/conference/server/PermissionManager.java
@@ -0,0 +1,109 @@
+/*
+ * NAESC Conference is a Google App Engine web application that provides
+ * a conference registration system.
+ * Copyright (C) 2010  Speed School Student Council
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.naesc2011.conference.server;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.naesc2011.conference.shared.PMF;
+
+public class PermissionManager {
+	/**
+	 * 
+	 */
+	private static final Logger log = Logger.getLogger(PermissionManager.class.getName());
+	
+	/**
+	 * The user instance, if the user was logged in.
+	 */
+	private User currentUser;
+	
+	/**
+	 * 
+	 */
+	private PermissionUserInstance currentPermissions;
+	
+	/**
+	 * 
+	 */
+	public PermissionManager(){
+		UserService userService = UserServiceFactory.getUserService();
+	    this.currentUser = userService.getCurrentUser();
+
+	    // Get the user permissions from the datastore.
+	    if(this.currentUser != null){
+		    PersistenceManager pm = PMF.get().getPersistenceManager();
+		    Query query = pm.newQuery(PermissionUserInstance.class);
+		    query.setFilter("userId == userIdParam");
+		    query.declareParameters("String userIdParam");
+			try{
+				List<PermissionUserInstance> results = (List<PermissionUserInstance>)query.execute(this.currentUser.getUserId());
+				if(results.size() == 1){
+					this.currentPermissions = results.get(0);
+					log.info("Located the user's permissions." + this.currentPermissions.getUserPermission());
+				}
+				else if(results.size() == 0){
+					// Put the data into the datastore
+					this.currentPermissions = new PermissionUserInstance(this.currentUser, PermissionUserInstance.Permission.AUTHENTICATED);
+					pm.makePersistent(this.currentPermissions);
+					log.info("User permissions not found, defaulting to " + this.currentPermissions.getUserPermission());
+				}
+				else{
+					log.warning("Multiple entries in the datastore exist for " + this.currentUser.getUserId());
+				}
+		    } 
+			finally {
+		        query.closeAll();
+		    }
+			
+			pm.close();
+	    }
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Boolean IsUserLoggedIn(){
+		if(currentUser == null){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public PermissionUserInstance.Permission GetPermissionLevel(){
+		if(this.currentPermissions != null){
+			return this.currentPermissions.getUserPermission();
+		}
+		else{
+			return PermissionUserInstance.Permission.UNAUTHENTICATED;
+		}
+	}
+}
---
+++ src/com/naesc2011/conference/server/PermissionUserInstance.java
@@ -0,0 +1,106 @@
+/*
+ * NAESC Conference is a Google App Engine web application that provides
+ * a conference registration system.
+ * Copyright (C) 2010  Speed School Student Council
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.naesc2011.conference.server;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.naesc2011.conference.shared.CorporateCompany;
+
+@PersistenceCapable
+public class PermissionUserInstance {
+	/**
+	 * 
+	 */
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+	
+	/**
+	 * 
+	 */
+	@Persistent
+	private String userId;
+	
+	/**
+	 * 
+	 */
+	@Persistent
+	private Permission userPermission;
+	
+	/**
+	 * 
+	 */
+	public enum Permission{
+		UNAUTHENTICATED,
+		AUTHENTICATED,
+		USER,
+		MANAGER,
+		ADMIN
+	}
+	
+	/**
+	 * 
+	 */
+	public PermissionUserInstance(User user, Permission userPermission){
+		this.userId = user.getUserId();
+		this.userPermission = userPermission;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Key getKey(){
+		return this.key;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the userPermission
+	 */
+	public Permission getUserPermission() {
+		return userPermission;
+	}
+
+	/**
+	 * @param userPermission the userPermission to set
+	 */
+	public void setUserPermission(Permission userPermission) {
+		this.userPermission = userPermission;
+	}
+}
