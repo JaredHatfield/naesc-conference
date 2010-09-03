b617dc02acb06265bf583f32eca6e8fa6830443f
jjhatf02 <jjhatf02@c9196558-8581-6202-ea55-1f6dc3d4705e>
2010-09-03 20:09:49 +0000

    Added all of the necessary JSP files and servlet classes that will be used to create the application.

---
+++ src/com/naesc2011/conference/server/naesc/ProcessSaveCouncilServlet.java
@@ -0,0 +1,22 @@
+/*
+ * NAESC Conference is a Google App Engine web application that provides
+ * a conference registration system.
+ * Copyright (C) 2010  Speed School Student Council
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.naesc2011.conference.server.naesc;
+
+public class ProcessSaveCouncilServlet {
+
+}
