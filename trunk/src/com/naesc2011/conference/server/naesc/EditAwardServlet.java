c38551ba142c5c7469c8e8346901a6ff83163230
jjhatf02 <jjhatf02@c9196558-8581-6202-ea55-1f6dc3d4705e>
2010-09-21 02:56:34 +0000

    Comment issue 8 Filled in the servlet shell that will be used for award processing.  The actual logic is not implemented by this code, rather this provides the framework to be filled in.

--- trunk/src/com/naesc2011/conference/server/naesc/EditAwardServlet.java
+++ trunk/src/com/naesc2011/conference/server/naesc/EditAwardServlet.java
@@ -18,7 +18,9 @@
 package com.naesc2011.conference.server.naesc;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.jdo.PersistenceManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -27,6 +29,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.naesc2011.conference.server.PermissionManager;
+import com.naesc2011.conference.shared.Council;
+import com.naesc2011.conference.shared.CouncilPermission;
+import com.naesc2011.conference.shared.PMF;
 
 public class EditAwardServlet extends HttpServlet {
 
@@ -44,10 +49,37 @@ public class EditAwardServlet extends HttpServlet {
         boolean authenticated = PermissionManager.SetUpPermissions(p, request);
 
         if (authenticated) {
-            String url = "/naesc/editaward.jsp";
-            ServletContext context = getServletContext();
-            RequestDispatcher dispatcher = context.getRequestDispatcher(url);
-            dispatcher.forward(request, response);
+            String pid = request.getParameter("id");
+            if (pid != null) {
+                long id = Long.parseLong(pid);
+                PersistenceManager pm = PMF.get().getPersistenceManager();
+
+                List<CouncilPermission> councils = CouncilPermission
+                        .GetPermission(pm, p.getUser().getUserId());
+                Boolean haspermission = false;
+                for (int i = 0; i < councils.size(); i++) {
+                    if (councils.get(i).getCouncil().getId() == id) {
+                        haspermission = true;
+                        break;
+                    }
+                }
+
+                if (haspermission) {
+                    Council council = Council.GetCouncil(pm, pid);
+
+                    // TODO: Set the specified award as the attribute
+
+                    String url = "/naesc/editaward.jsp";
+                    ServletContext context = getServletContext();
+                    RequestDispatcher dispatcher = context
+                            .getRequestDispatcher(url);
+                    dispatcher.forward(request, response);
+                } else {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                }
+            } else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
         } else {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
