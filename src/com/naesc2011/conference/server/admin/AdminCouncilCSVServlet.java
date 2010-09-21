4357326d695b75c6f67086a90f8d8f71a8cce19a
jjhatf02 <jjhatf02@c9196558-8581-6202-ea55-1f6dc3d4705e>
2010-09-21 03:06:28 +0000

    Comment issue 10 Added the necessary servlets for displaying the council and attendee CSV files.  The logic still needs to be added to display the actual information.

---
+++ src/com/naesc2011/conference/server/admin/AdminCouncilCSVServlet.java
@@ -0,0 +1,49 @@
+/*
+ * NAESC Conference is a Google App Engine web application that provides
+ * a conference registration system.
+ * Copyright (C) 2010  Speed School Student Council
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.naesc2011.conference.server.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.naesc2011.conference.server.PermissionManager;
+
+public class AdminCouncilCSVServlet extends HttpServlet {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PermissionManager p = new PermissionManager();
+        boolean authenticated = PermissionManager.SetUpPermissions(p, request);
+
+        if (authenticated) {
+            // TODO: Display all of the council information in CSV format
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
