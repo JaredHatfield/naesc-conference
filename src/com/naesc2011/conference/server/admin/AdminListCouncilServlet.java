c1455b0f878d0aa17bb10e826efd66a74784e8c2
jjhatf02 <jjhatf02@c9196558-8581-6202-ea55-1f6dc3d4705e>
2010-09-21 02:38:24 +0000

    Update issue 9 Added the necessary files to display the council admin interface.  The council's are not actually displayed, but this provides the files that need to be modified.

---
+++ src/com/naesc2011/conference/server/admin/AdminListCouncilServlet.java
@@ -0,0 +1,56 @@
+/*
+ * NAESC Conference is a Google App Engine web application that provides
+ * a conference registration system.
+ * Copyright (C) 2010  Speed School Student Council
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.naesc2011.conference.server.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.naesc2011.conference.server.PermissionManager;
+
+public class AdminListCouncilServlet extends HttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PermissionManager p = new PermissionManager();
+        boolean authenticated = PermissionManager.SetUpPermissions(p, request);
+
+        if (authenticated) {
+            // TODO: Implement the logic for retrieving all of the councils.
+            String url = "/admin/listcouncil.jsp";
+            ServletContext context = getServletContext();
+            RequestDispatcher dispatcher = context.getRequestDispatcher(url);
+            dispatcher.forward(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
